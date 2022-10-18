9f9c8ffa9bd3995578573e539240ab305c767ab2
sunqingzheng@gocyber.world <sunqingzheng@gocyber.world>
2022-10-18 23:46:33 +0800

    相机矩阵完成

---
+++ lesson2/camera.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+axis_x = [1,0,0]
+axis_y = [0,1,0]
+axis_z = [0,0,1]
+
+
+class Camera:
+    def __init__(self):
+        #  相机位置 
+        self.position = [0, 0, 3]
+        #  用来计算相机朝向 
+        self.look_at = [0, 0, 0]
+        #  相机向上的轴（计算相机旋转） 
+        self.up = axis_y
+        #  视角 
+        self.fov = 45
+        #  近平面位置 
+        self.near = 1
+        #  远平面 
+        self.far = 1000
+        #  视图比例 
+        self.aspect = 1
+        #  宽高（视图） 
+        self.height = 1
+        self.width = 1
+    
+    def set(self, position, look_at, up, fov, near):
+        if (np.array_equal(position, look_at)):
+            print ("failed: position is equal to look_at")
+            return
+
+        if (fov <= 0 or fov >= 180):
+            self.fov = (fov + 180 ) % 180
+        else:
+            self.fov = fov
+
+        self.position = position
+        self.look_at = look_at
+        self.up = up
+        self.near = near
+
+
+    #  "世界->相机空间矩阵 https://zhuanlan.zhihu.com/p/561394626 
+    def world_to_camera_matrix(self):
+        n = self.norm(np.subtract(self.look_at,self.position))
+        up = self.norm(self.up)
+        #  up与朝向在同一轴上,点积为 +-1,选取x轴作为相机up 
+        if(np.abs(np.dot(n,up) == 1)):
+            self.up = [np.dot(n,up),0,0]
+            if(np.abs(np.dot(n,up) == 1)):
+                self.up = [0,0,np.dot(n,up)]
+        
+        v = self.norm(np.cross(n,up))
+        u = self.norm(np.cross(v,n))
+        #  旋转矩阵的齐次矩阵 
+        rot = np.array([
+
+            [v[0],v[1],v[2],0],
+            [u[0],u[1],u[2],0],
+            [n[0],n[1],n[2],0],
+            [0,0,0,1]
+        ])
+        tran = np.array([
+            [1,0,0,self.position[0]],
+            [0,1,0,self.position[1]],
+            [0,0,1,self.position[2]],
+            [0,0,0,1],
+        ])
+        return np.matmul(rot,tran)
+
+    #  透视视图矩阵 
+    def perspective_matrix(self):
+        return np.matmul(self.M_perspective(),self.world_to_camera_matrix())
+
+    # 正交视图矩阵 
+    def orthographic_matrix(self):
+        return np.matmul(self.M_orthographic(),self.world_to_camera_matrix())
+
+    # 正交投影矩阵
+    def M_orthographic(self):
+        return np.array([
+            [1,0,0,0],
+            [0,1,0,0],
+            [0,0,0,0],
+            [0,0,0,1],
+        ])
+
+    # 透视投影矩阵 https://zhuanlan.zhihu.com/p/359128442
+    def M_perspective(self):
+        self.getPerspectiveHalfSize()
+        return np.array([
+            [self.near*2/self.height, 0, 0, 0],
+            [0, self.near*2/self.width, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, -1, 0]
+        ])
+    
+    def getPerspectiveHalfSize(self):
+        self.width = self.near * np.tan(self.fov * np.pi / 360) *2
+        self.height = self.width / self.aspect
+
+    def norm(self,v):
+        norm = np.linalg.norm(v)
+        if norm == 0: 
+            return v
+        return v / norm
---
+++ lesson2/rasterizer.py
@@ -0,0 +1,184 @@
+import numpy as np
+import struct
+from PIL import Image
+from pygltflib import GLTF2
+
+from camera import Camera
+
+I_WIDTH = 800
+I_HEIGHT = 600
+I_SCALE = 200
+RGBS = np.zeros(I_WIDTH * I_HEIGHT * 3, np.uint8).reshape(I_HEIGHT, I_WIDTH, 3)
+WHITE = (255,255,255)
+
+camera = Camera()
+camera.set(
+    position=[1, 10, 1],
+    look_at=[0, 0, 0],
+    up=[0, 1, 0],
+    fov=45,
+    near=1
+)
+
+
+CAMERA_MATRIX = camera.orthographic_matrix().transpose()
+
+def drawPixels():
+
+    i = Image.fromarray(RGBS, 'RGB')
+    i.show()
+
+def loadGltf(path):
+    gltf = GLTF2().load(path)
+    mesh = gltf.meshes[gltf.scenes[gltf.scene].nodes[0]]
+    primitives = []
+
+    # get the vertices for each primitive in the mesh (in this example there is only one)
+    for primitive in mesh.primitives:
+
+        # get all vertices
+        # get the binary data for this mesh primitive from the buffer
+        accessor = gltf.accessors[primitive.attributes.POSITION]
+        bufferView = gltf.bufferViews[accessor.bufferView]
+        buffer = gltf.buffers[bufferView.buffer]
+        data = gltf.get_data_from_buffer_uri(buffer.uri)
+        dataLength = (int)(bufferView.byteLength / accessor.count)
+        
+        # pull each vertex from the binary buffer and convert it into a tuple of python floats
+        vertices = []
+        for i in range(accessor.count):
+            index = bufferView.byteOffset + accessor.byteOffset + i * dataLength  # the location in the buffer of this vertex
+            d = data[index:index + dataLength]  # the vertex data
+            v = struct.unpack("<fff", d)   # convert from base64 to three floats
+            vertices.append(v)
+
+        # get all normals
+        # get the binary data for this mesh primitive from the buffer
+        accessor = gltf.accessors[primitive.attributes.NORMAL]
+        bufferView = gltf.bufferViews[accessor.bufferView]
+        buffer = gltf.buffers[bufferView.buffer]
+        data = gltf.get_data_from_buffer_uri(buffer.uri)
+        dataLength = (int)(bufferView.byteLength / accessor.count)
+
+        # pull each vertex from the binary buffer and convert it into a tuple of python floats
+        normals = []
+        for i in range(accessor.count):
+            index = bufferView.byteOffset + accessor.byteOffset + i * dataLength  # the location in the buffer of this vertex
+            d = data[index:index + dataLength]
+            v = struct.unpack("<fff", d)   # convert from base64 to three floats
+            normals.append(v)
+
+        # get all uvs
+        # get the binary data for this mesh primitive from the buffer
+        accessor = gltf.accessors[primitive.attributes.TEXCOORD_0]
+        bufferView = gltf.bufferViews[accessor.bufferView]
+        buffer = gltf.buffers[bufferView.buffer]
+        data = gltf.get_data_from_buffer_uri(buffer.uri)
+        dataLength = (int)(bufferView.byteLength / accessor.count)
+
+        # pull each vertex from the binary buffer and convert it into a tuple of python floats
+        uvs = []
+        for i in range(accessor.count):
+            index = bufferView.byteOffset + accessor.byteOffset + i * dataLength  # the location in the buffer of this vertex
+            uv = data[index:index + dataLength]
+            value = struct.unpack("ff", uv)   # convert from base64 to two floats
+            uvs.append(value)
+
+        # get all indices
+        # get the binary data for this mesh primitive from the buffer
+        accessor = gltf.accessors[primitive.indices]
+        bufferView = gltf.bufferViews[accessor.bufferView]
+        buffer = gltf.buffers[bufferView.buffer]
+        data = gltf.get_data_from_buffer_uri(buffer.uri)
+        dataLength = (int)(bufferView.byteLength / accessor.count)
+
+        # pull each vertex from the binary buffer and convert it into a tuple of python floats
+        indices = []
+        for i in range(accessor.count):
+            index = bufferView.byteOffset + accessor.byteOffset + i * dataLength  # the location in the buffer of this vertex
+            indice = data[index:index + dataLength]
+            val = struct.unpack("h", indice)
+            indices.append(val)
+
+        primitives.append({'vertices':vertices,
+            'normals': normals,
+            'uvs': uvs,
+            'indices': indices
+        })
+    return primitives
+
+
+print(CAMERA_MATRIX)
+
+def rasterizeLine(startf, endf):
+    start = np.matmul(np.append(startf ,1),CAMERA_MATRIX)
+    end = np.matmul( np.append(endf,1),CAMERA_MATRIX )
+    # print(start,end)
+    drawLine(positionToPixelPos(start), positionToPixelPos(end))
+
+def drawPixel(position, color):
+    x = position[0]
+    y = position[1]
+    if x>=I_WIDTH or x<0 or y>=I_HEIGHT or y<0:
+        return
+
+    RGBS[y][x][0] = color[0]
+    RGBS[y][x][1] = color[1]
+    RGBS[y][x][2] = color[2]
+
+def positionToPixelPos(position):
+    x = position[0]/position[3]
+    y = position[1]/position[3]
+    x1 = (int)(x * I_SCALE + I_WIDTH/2)
+    y1 = (int)(-1 * y * I_SCALE + I_HEIGHT/2)
+    return (x1, y1)
+
+def drawLine(start, end):
+    
+    startx = start[0]
+    starty = start[1]
+    endx = end[0]
+    endy = end[1]
+
+    step = 1
+    if(startx > endx):
+        step = -1
+    if endx!=startx:
+        for x in range(startx, endx, step):
+            y = starty + (int)((endy - starty)*(x - startx)/(endx - startx))
+            drawPixel((x,y), WHITE)
+
+    step = 1
+    if(starty > endy):
+        step = -1
+    if endy!=starty:
+        for y in range(starty, endy, step):
+            x = startx + (int)((endx - startx)*(y - starty)/(endy - starty))
+            drawPixel((x,y), WHITE)
+    drawPixel((endx,endy), WHITE)
+
+def norm(v):
+    norm = np.linalg.norm(v)
+    if norm == 0: 
+       return v
+    return v / norm
+
+    
+
+data = loadGltf("monkey.gltf")
+
+
+
+for p in data:
+    positions = p["vertices"]
+    indices = p["indices"]
+    
+    for i in range(0, len(indices), 3):
+        a = positions[indices[i][0]]
+        b = positions[indices[i + 1][0]]
+        c = positions[indices[i + 2][0]]
+        rasterizeLine(a, b)
+        rasterizeLine(b, c)
+        rasterizeLine(c, a)
+
+drawPixels()
